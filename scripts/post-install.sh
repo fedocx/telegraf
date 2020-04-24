00ad276696ea777bf12210f03f5b275191d7f35e
kirt <fedocx2009@gmail.com>
2020-04-24 21:32:50 +0800

    add rpm file

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -46,6 +46,7 @@ fi
 # If 'telegraf.conf' is not present use package's sample (fresh install)
 if [[ ! -f /etc/telegraf/telegraf.conf ]] && [[ -f /etc/telegraf/telegraf.conf.sample ]]; then
    cp /etc/telegraf/telegraf.conf.sample /etc/telegraf/telegraf.conf
+   chmod 666 /etc/telegraf/telegraf.conf
 fi
 
 # Distribution-specific logic
